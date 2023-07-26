ddd7ee3043e3ec297f8e1c341fc3ffcc62352eec
Alex23603 <Alexx24603@gmail.com>
2023-07-26 14:15:32 -0700

    update1

--- shell_loop.c
+++ shell_loop.c
@@ -1,92 +1,136 @@
 #include "shell.h"
 
 /**
- * strtow2 - Splits a string into an array of words (with custom delimiters)
- * @str: The input string to be split
- * @delims: The delimiters used for splitting
+ * strtow2 - Function to split a string into words based on given delimiters.
+ * @str: Input string to split.
+ * @delims: Delimiters to split the string.
  *
- * Return: Pointer to an array of strings (words)
+ * Return: An array of strings (words).
  */
 char **strtow2(char *str, char *delims)
 {
-	int i, j, k, word_count = 0, len;
+	int i, j, k = 0;
+	int word_count = 0;
 	char **words;
 
-	if (str == NULL || str[0] == '\0')
+	if (!str || !delims)
 		return (NULL);
 
-	len = _strlen(str);
-	words = malloc((len + 1) * sizeof(char *));
-	if (words == NULL)
+	word_count = count_words(str, delims);
+	if (word_count == 0)
 		return (NULL);
 
-	for (i = 0, k = 0; str[i]; i++)
+	words = malloc((word_count + 1) * sizeof(char *));
+	if (!words)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
 	{
-		if (!_strchr(delims, str[i]))
-		{
-			for (j = i; str[j] && !_strchr(delims, str[j]); j++)
-				;
-			words[k] = malloc((j - i + 1) * sizeof(char));
-			if (words[k] == NULL)
-			{
-				for (k--; k >= 0; k--)
-					free(words[k]);
-				free(words);
-				return (NULL);
-			}
+		if (is_delim(str[i], delims))
+			continue;
 
-			for (; i < j; i++)
-				words[k][i - k] = str[i];
-			words[k++][i - k] = '\0';
-			word_count++;
+		for (j = i; str[j] && !is_delim(str[j], delims); j++)
+			;
+
+		words[k] = malloc((j - i + 1) * sizeof(char));
+		if (!words[k])
+		{
+			ffree(words);
+			return (NULL);
 		}
+
+		for (; i < j; i++)
+			words[k][i - k] = str[i];
+		words[k++][i - k] = '\0';
 	}
 
-	words[word_count] = NULL;
+	words[k] = NULL;
 	return (words);
 }
 
-int loophsh(char **env)
+/**
+ * count_words - Function to count the number of words in a string.
+ * @str: Input string.
+ * @delims: Delimiters to check for word separation.
+ *
+ * Return: The number of words in the string.
+ */
+static int count_words(char *str, char *delims)
 {
-	info_t info = INFO_INIT;
+	int i, count = 0;
+	int is_word = 0;
 
-	/* Initialize the history list */
-	info.history = add_node_end(&info.history, NULL, 0);
-
-	/* Set the initial values */
-	info.argv = NULL;
-	info.environ = env;
-	info.status = 0;
-
-	/* Ignore SIGINT (Ctrl+C) to prevent termination */
-	signal(SIGINT, sigintHandler);
-
-	/* Read commands from stdin and execute them */
-	while (get_input(&info) != -1)
+	for (i = 0; str[i]; i++)
 	{
-		if (info.arg && *info.arg)
+		if (!is_delim(str[i], delims))
 		{
-			info.arg = replace_alias(&info);
-			if (!info.arg || !_strcmp(info.arg, ""))
+			if (!is_word)
 			{
-				free(info.arg);
-				continue;
+				count++;
+				is_word = 1;
 			}
-
-			/* Tokenize the input command */
-			info.argv = strtow2(info.arg, " \t\r\n\a");
-			if (!info.argv)
-			{
-				free(info.arg);
-				continue;
-			}
-
-			/* Find and execute the command */
-			find_cmd(&info);
-			free_info(&info, 0);
+		}
+		else
+		{
+			is_word = 0;
 		}
 	}
-	write_history(&info);
-	free_list(&info.history);
-	return (info.status);
+
+	return (count);
+}
+
+/**
+ * is_delim - Function to check if a character is a delimiter.
+ * @c: The character to check.
+ * @delims: The string containing delimiters.
+ *
+ * Return: 1 if the character is a delimiter, otherwise 0.
+ */
+static int is_delim(char c, char *delims)
+{
+	int i;
+
+	for (i = 0; delims[i]; i++)
+	{
+		if (c == delims[i])
+			return (1);
+	}
+
+	return (0);
+}
+
+/**
+ * loophsh - The main loop of the shell.
+ * @environ: The array of environment variables.
+ *
+ * Return: 0 on success, otherwise 1.
+ */
+int loophsh(char **environ)
+{
+	info_t info;
+	int exit_status = 0;
+
+	set_info(&info, environ);
+
+	do {
+		if (info.linecount_flag)
+			info.line_count++;
+
+		if (get_input(&info) == EOF)
+			break;
+
+		if (info.arg[0] != '\n' && info.arg[0] != 0)
+		{
+			info.argv = strtow2(info.arg, " \t\r\n\a");
+			if (info.argv == NULL)
+				continue;
+			find_cmd(&info);
+		}
+		free_info(&info, 1);
+	} while (exit_status == 0);
+
+	free_info(&info, 1);
+	free_list(&(info.env));
+
+	return (exit_status);
 }
